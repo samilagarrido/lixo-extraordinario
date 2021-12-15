ad8b7ecc603bdf8c0d6a68ef9459c82dc72184fa
samgrs <samilagrodrigues@hotmail.com>
2021-12-15 12:05:31 -0300

    atualiza rotas

--- src/routes/placesRoutes.js
+++ src/routes/placesRoutes.js
@@ -4,9 +4,9 @@ const controller = require("../controller/placesController.js");
 
 router.get("/all", controller.getAll);
 router.post("/create", controller.createPlace);  
-router.get("/search:id", controller.getById);
-router.patch("/update:id", controller.updatePlace);
-router.delete("/delete:id", controller.deletePlace);
+router.get("/search/:id", controller.getById);
+router.patch("/update/:id", controller.updatePlace);
+router.delete("/delete/:id", controller.deletePlace);
 
 module.exports = router
 
